6182e7dcab35380877e6b0ca82c7280704188f23
Juan Diego Caballero <jd_caballero@hotmail.com>
2013-01-27 15:14:07 +0100

    Minor adjustments to template showing the clicks

--- test_heatmap/django_heatmap/urls.py
+++ test_heatmap/django_heatmap/urls.py
@@ -9,7 +9,8 @@ from djcelery.views import is_task_successful
 urlpatterns = patterns('',
 	url(r'^$',ListView.as_view(queryset=Page.objects.all(),context_object_name='page_list',template_name='page_list.html'),name='index'),
     url(r'^show_clicks/(?P<res_x>\d+)/(?P<res_y>\d+)/(?P<height>\d+)/(?P<width>\d+)/(?P<url>.+)/$', show_click_events, name='show_click_events'), 
-    url(r'^save_click_event/$', save_click_event, name='save_click_event'), 
+    url(r'^get_clicks/(?P<res_x>\d+)/(?P<res_y>\d+)/(?P<height>\d+)/(?P<width>\d+)/(?P<url>.+)/$', show_click_events, name='get_click_events'), 
+	url(r'^save_click_event/$', save_click_event, name='save_click_event'), 
 
 	url(r'^test_events/$',test_events,name='test_events'),    
 )
